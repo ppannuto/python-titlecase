6b0c1ecd8e5bed4e76486ecdcb00e896a3f5a665
Stuart Colville <muffinresearchlabs@gmail.com>
2008-10-31 16:12:26 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(
+    name='titlecase',
+    version='0.3',
+    py_modules=['titlecase'],
+)
--- titlecase.py
+++ titlecase.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-titlecase.py v0.2
+titlecase.py v0.3
 Original Perl version by: John Gruber http://daringfireball.net/ 10 May 2008
 Python version by Stuart Colville http://muffinresearch.co.uk
 License: http://www.opensource.org/licenses/mit-license.php
