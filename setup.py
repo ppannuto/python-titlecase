5b32f56efceb3133bf3e95e3a22b399a43ea9671
Stuart Colville <muffinresearch@gmail.com>
2009-07-24 00:58:37 +0100

    prepping v0.4 moved into it's own directory and fixed moved tests out

--- setup.py
+++ setup.py
@@ -1,7 +1,37 @@
-from distutils.core import setup
+__version__ = '0.4'
 
-setup(
-    name='titlecase',
-    version='0.3',
-    py_modules=['titlecase'],
+import os
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README')).read()
+
+setup(name='titlecase',
+    version=__version__,
+    description="Python Port of John Gruber's titlecase.pl",
+    long_description=README,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Topic :: Text Processing :: Filters",
+    ],
+    keywords='string formatting',
+    author="Stuart Colville",
+    author_email="pypi@muffinresearch.co.uk",
+    url="http://www.muffinresearch.co.uk/",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    test_suite="titlecase.tests",
+    entry_points = """\
+    """
 )
+
--- titlecase/tests.py
+++ titlecase/tests.py
@@ -1,68 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-titlecase.py v0.3
-Original Perl version by: John Gruber http://daringfireball.net/ 10 May 2008
-Python version by Stuart Colville http://muffinresearch.co.uk
-License: http://www.opensource.org/licenses/mit-license.php
-"""
-
 import unittest
-import sys
-import re
+from titlecase import titlecase
 
-
-SMALL = 'a|an|and|as|at|but|by|en|for|if|in|of|on|or|the|to|v\.?|via|vs\.?'
-PUNCT = "[!\"#$%&'‘()*+,-./:;?@[\\\\\\]_`{|}~]"
-
-SMALL_WORDS = re.compile(r'^(%s)$' % SMALL, re.I)
-INLINE_PERIOD = re.compile(r'[a-zA-Z][.][a-zA-Z]')
-UC_ELSEWHERE = re.compile(r'%s*?[a-zA-Z]+[A-Z]+?' % PUNCT)
-CAPFIRST = re.compile(r"^%s*?([A-Za-z])" % PUNCT)
-SMALL_FIRST = re.compile(r'^(%s*)(%s)\b' % (PUNCT, SMALL), re.I)
-SMALL_LAST = re.compile(r'\b(%s)%s?$' % (SMALL, PUNCT), re.I)
-SUBPHRASE = re.compile(r'([:.;?!][ ])(%s)' % SMALL)
-
-def titlecase(text):
-
-    """
-    Titlecases input text
-
-    This filter changes all words to Title Caps, and attempts to be clever
-    about *un*capitalizing SMALL words like a/an/the in the input.
-
-    The list of "SMALL words" which are not capped comes from
-    the New York Times Manual of Style, plus 'vs' and 'v'.
-
-    """
-
-    words = re.split('\s', text)
-    line = []
-    for word in words:
-        if INLINE_PERIOD.search(word) or UC_ELSEWHERE.match(word):
-            line.append(word)
-            continue
-        if SMALL_WORDS.match(word):
-            line.append(word.lower())
-            continue
-        line.append(CAPFIRST.sub(lambda m: m.group(0).upper(), word))
-
-    line = " ".join(line)
-
-    line = SMALL_FIRST.sub(lambda m: '%s%s' % (
-        m.group(1),
-        m.group(2).capitalize()
-    ), line)
-
-    line = SMALL_LAST.sub(lambda m: m.group(0).capitalize(), line)
-
-    line = SUBPHRASE.sub(lambda m: '%s%s' % (
-        m.group(1),
-        m.group(2).capitalize()
-    ), line)
-
-    return line
 
 class TitlecaseTests(unittest.TestCase):
 
@@ -243,12 +184,7 @@ class TitlecaseTests(unittest.TestCase):
         self.assertEqual(text, result, "%s should be: %s" % (text, result, ))
 
 
-if __name__ == '__main__':
-    if not sys.stdin.isatty():
-        for line in sys.stdin:
-            print titlecase(line)
-
-    else:
-        suite = unittest.TestLoader().loadTestsFromTestCase(TitlecaseTests)
-        unittest.TextTestRunner(verbosity=2).run(suite)
+if __name__ == "__main__":
+    suite = unittest.TestLoader().loadTestsFromTestCase(TitlecaseTests)
+    unittest.TextTestRunner(verbosity=2).run(suite)
 
