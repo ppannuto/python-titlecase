e207030a4591e080991906b82f9484ae525b876f
Stuart Colville <stuart.colville@canonical.com>
2010-07-09 00:02:23 +0100

    fix newline replacement

--- titlecase/tests.py
+++ titlecase/tests.py
@@ -123,6 +123,18 @@ TEST_DATA = (
         "Mr McTavish went to MacDonalds",
         "Mr McTavish Went to MacDonalds"
     ),
+    (
+        "this shouldn't\nget mangled",
+        "This Shouldn't\nGet Mangled"
+    ),
+    #(
+        #"foo/bar",
+        #"Foo/Bar"
+    #),
+    ( 
+        "this is http://foo.com",
+        "This Is http://foo.com"
+    )
 )
 
 def test_all_caps_regex():
