f78cfc708030ad508636c5c8ec0f4d20e52f1009
Pat Pannuto <pat.pannuto@gmail.com>
2014-07-31 14:45:04 -0400

    Point to right URL/e-mail. Bump to 0.7.1
    It seems I messed up deploying to pypi somehow. Let's try to fix
    that with this commit.

--- titlecase/__init__.py
+++ titlecase/__init__.py
@@ -10,7 +10,7 @@ License: http://www.opensource.org/licenses/mit-license.php
 import re
 
 __all__ = ['titlecase']
-__version__ = '0.7.0'
+__version__ = '0.7.1'
 
 SMALL = 'a|an|and|as|at|but|by|en|for|if|in|of|on|or|the|to|v\.?|via|vs\.?'
 PUNCT = r"""!"#$%&'‘()*+,\-./:;?@[\\\]_`{|}~"""
