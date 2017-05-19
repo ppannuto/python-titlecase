7f31192c0936224dc0810d84ac6bd6131376e61e
Pat Pannuto <pat.pannuto@gmail.com>
2017-05-19 11:38:57 -0400

    Bump to version 0.10.0
      * #24 from @AlejandroFrias: Mark callback strings immutable
      * #25 from @Garret-R: Handle dashes — unicode codepoints 2012 through 2016

--- titlecase/__init__.py
+++ titlecase/__init__.py
@@ -12,7 +12,7 @@ import re
 import sys
 
 __all__ = ['titlecase']
-__version__ = '0.9.0'
+__version__ = '0.10.0'
 
 SMALL = 'a|an|and|as|at|but|by|en|for|if|in|of|on|or|the|to|v\.?|via|vs\.?'
 PUNCT = r"""!"#$%&'‘()*+,\-–‒—―./:;?@[\\\]_`{|}~"""
