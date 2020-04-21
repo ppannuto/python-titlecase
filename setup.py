22d262cb4b74d635b441561ee0b9a34d2066dff5
Alex Cabal <alex@alexcabal.com>
2020-04-21 16:42:36 -0500

    Add regex to tests

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(name='titlecase',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    tests_require=['nose'],
+    tests_require=['nose', 'regex'],
     setup_requires=['nose>=1.0', 'regex>=2020.4.4'],
     test_suite="titlecase.tests",
     entry_points = {
