c1855c514a55ca1964e187b0e862a954da4ca3d3
Pat Pannuto <pat.pannuto@gmail.com>
2022-08-15 12:59:27 -0700

    v2.4
     * Add `preserve_blank_lines` option (#88)
     * Add Py3.10, Drop Py3.6
     * Update unit testing framework

--- titlecase/__init__.py
+++ titlecase/__init__.py
@@ -23,7 +23,7 @@ else:
     REGEX_AVAILABLE = True
 
 __all__ = ['titlecase']
-__version__ = '2.3.0'
+__version__ = '2.4.0'
 
 SMALL = r'a|an|and|as|at|but|by|en|for|if|in|of|on|or|the|to|v\.?|via|vs\.?'
 PUNCT = r"""!"“#$%&'‘()*+,\-–‒—―./:;?@[\\\]_`{|}~"""
